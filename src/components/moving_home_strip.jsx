26796834e278507690290a47ef0f3b1831fcb2d5
akt9999 <athotakuras@gmail.com>
2026-03-16 19:20:36 +0000

    usefast-maruqee for a lot better of a ticker could use styling but thats for later.

--- src/components/moving_home_strip.jsx
+++ src/components/moving_home_strip.jsx
@@ -1,42 +1,30 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
+import Marquee from "react-fast-marquee";
 import "../css/moving_home_strip.css";
 
 export default function MovingHomeStrip() {
-  const items = [
-    "features resources 1",
-    "features resources 2",
-    "features resources 3",
-    "features resources 4",
-    "features resources 5",
-    "features resources 6",
-    "features resources 7",
-    "features resources 8",
+  const resources = [
+    "Resource 1",
+    "Resource 2",
+    "Resource 3",
+    "Resource 4",
+    "Resource 5",
+    "Resource 6",
+    "Resource 7",
+    "Resource 8",
+    "Resource 9",
+    "Resource 10"
   ];
-
-  const firstSetRef = useRef(null);
-  const trackRef = useRef(null);
-
-  useEffect(() => {
-    if (firstSetRef.current && trackRef.current) {
-      const singleSetWidth = firstSetRef.current.scrollWidth;
-      trackRef.current.style.setProperty("--single-set-width", `${singleSetWidth}px`);
-    }
-  }, []);
 
   return (
     <div className="ticker">
-      <div className="ticker-track" ref={trackRef}>
-        <div className="ticker-set" ref={firstSetRef}>
-          {items.map((item, index) => (
-            <span key={index} className="ticker-item">{item}</span>
-          ))}
-        </div>
-        <div className="ticker-set" aria-hidden="true">
-          {items.map((item, index) => (
-            <span key={index} className="ticker-item">{item}</span>
-          ))}
-        </div>
-      </div>
+      <Marquee speed = {80} pauseOnHover = {true}>
+        {resources.map((item, i) => (
+          <span key={i} className="ticker-item">
+            {item}
+          </span>
+        ))}
+      </Marquee>
     </div>
   );
 }
